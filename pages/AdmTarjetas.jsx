398bfe1b401ee8ea83b056d2247558be64cd707d
NGutierrezSteccato <107592561+NahueeGutierrez@users.noreply.github.com>
2023-09-30 11:05:42 -0300

    Update AdmTarjetas.jsx

--- pages/AdmTarjetas.jsx
+++ pages/AdmTarjetas.jsx
@@ -15,7 +15,7 @@ function CreditCardList({ creditCards }) {
   );
 }
 
-// Página principal que muestra la lista de tarjetas de crédito
+// Pagina principal que muestra la lista de tarjetas 
 function HomePage({ creditCards }) {
   return (
     <div>
@@ -25,11 +25,9 @@ function HomePage({ creditCards }) {
   );
 }
 export async function getStaticProps() {
-  // Simulación de obtención de tarjetas de crédito desde una API o base de datos
   const creditCards = [
     { id: '4798-2011-8481-4822', name: 'Tarjeta de Débito' },
     { id: '4213-7178-4718-6503', name: 'Tarjeta de Crédito' },
-    // Agrega más tarjetas aquí
   ];
 
   return {
